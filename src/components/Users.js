da538b2733aff84916a3428af1605e21b421cf0b
lumrachele <rlum894@gmail.com>
2019-02-23 17:36:13 -0500

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Users extends Component {
 
@@ -7,12 +8,24 @@ class Users extends Component {
       <div>
         <ul>
           Users!
+            {this.props.users.map((user)=>{
+              return <li key={user.index}>{user.username}</li>
+            })}
         </ul>
+        <p>
+        {this.props.userCount}
+        </p>
       </div>
     )
   }
 }
 
 //add mapStateToProps here
+function mapStateToProps(state){
+  return {users: state.users,
+    userCount: state.users.length
+  }
+}
 
-export default Users
+
+export default connect(mapStateToProps)(Users)
--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import App from './App'
 
 import manageUsers from './reducers/manageUsers'
 
+// import Redux from 'redux'
+import { createStore } from 'redux'
+import {Provider} from 'react-redux'
 
+const store = createStore(manageUsers)
 // add imports and code
 
 
 ReactDOM.render(
-  // add imports and code
+  <Provider store = { store }>
   <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
